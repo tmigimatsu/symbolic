c1c1fff2da70d7d2907956a39b9b75460684fa80
Toki Migimatsu <toki.migimatsu@gmail.com>
2020-05-13 11:28:37 -0700

    Add derived_predicate file

---
+++ include/symbolic/derived_predicate.h
@@ -0,0 +1,41 @@
+/**
+ * derived_predicate.h
+ *
+ * Copyright 2020. All Rights Reserved.
+ *
+ * Created: April 28, 2020
+ * Authors: Toki Migimatsu
+ */
+
+#ifndef SYMBOLIC_DERIVED_PREDICATE_H_
+#define SYMBOLIC_DERIVED_PREDICATE_H_
+
+#include "symbolic/action.h"
+
+namespace symbolic {
+
+class DerivedPredicate : public Action {
+ public:
+  DerivedPredicate(const Pddl& pddl, const VAL::derivation_rule* symbol);
+
+  const VAL::derivation_rule* symbol() const { return symbol_; }
+
+  const VAL::effect_lists* postconditions() const = delete;
+
+  bool Apply(State* state) const;
+
+  State Apply(const State& state, const std::vector<Object>& arguments) const = delete;
+
+  bool Apply(const std::vector<Object>& arguments, State* state) const = delete;
+
+  static State Apply(const State& state, const std::vector<DerivedPredicate>& predicates);
+
+  static bool Apply(const std::vector<DerivedPredicate>& predicates, State* state);
+
+ private:
+  const VAL::derivation_rule* symbol_;
+};
+
+}  // namespace symbolic
+
+#endif  // SYMBOLIC_DERIVED_PREDICATE_H_
